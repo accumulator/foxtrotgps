d569754fd3b20b12d938c55cde38b32c4d5af25b
Sander van Grieken <sander@outrightsolutions.nl>
2010-05-17 14:03:19 +0200

    view<-->world transformations were conflicting with OsmGpsMap transformation functions

--- src/converter.h
+++ src/converter.h
@@ -1,24 +1,3 @@
-float
-deg2rad(float deg);
-
-float
-rad2deg(float rad);
-
-int
-lat2pixel(	float zoom,
-		float lat);
-
-int
-lon2pixel(	float zoom,
-		float lon);
-		
-float
-pixel2lon(	float zoom,
-		int pixel_x);
-		
-float
-pixel2lat(	float zoom,
-		int pixel_y);
 char *
 latdeg2latmin(	float lat);
 
